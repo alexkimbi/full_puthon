c786bebac4443343dd1a1c92c0da652c5fbbaab7
Alex Kimbi <alexkimbi@hotmail.com>
2022-10-02 03:09:31 +0000

    Added the 4th line

--- bio.py
+++ bio.py
@@ -1,3 +1,4 @@
 name = input("what is your name? ")
 color = input("What is your favorite color? ")
 age = int(input("how old are you? "))
+print("your name is {name} and you are {color} and age of {age} ")
