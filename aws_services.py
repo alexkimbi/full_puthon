37f273e9fcf2a7a36ffab62ad5d3bfb203628c0c
Alex Kimbi <alexkimbi@hotmail.com>
2022-10-02 21:11:48 +0000

    commited

--- aws_services.py
+++ aws_services.py
@@ -2,7 +2,7 @@
 services_list = []
 services_list.append('ecr')
 print(services_list)
-#print("The lenght of services_list is:", len(services_list))
+print("The lenght of services_list is:", len(services_list))
 #services_list.remove('ecr')
 
 ###print("Deleting two specific services:")
