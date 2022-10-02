22c30a9510db89606aca1ea69603c6fe41b117c1
Alex Kimbi <alexkimbi@hotmail.com>
2022-10-02 04:21:20 +0000

    added print command

--- celcuis.py
+++ celcuis.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3.7
 
 #python implimentation goes here
-fahrenheit = float(input("What temp (in fahrenheit) would you like to convert to celcuis? "))
+fahrenheit = float(input("What temp (in fahrenheit) would you like to convert to celsuis? "))
 celsuis = (fahrenheit - 32) * 5 / 9
+
+print(fahrenheit, "F is",  celsuis, "C")
