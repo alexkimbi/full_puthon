9048e40f75b4f1619c14b4484025a23f22b155a8
Alex Kimbi <alexkimbi@hotmail.com>
2022-10-02 21:11:03 +0000

    commited

--- aws_services.py
+++ aws_services.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env pyhon3.7
 services_list = []
-services_list.append['ecr', 'ecc', 'lambda']
+services_list.append('ecr')
 print(services_list)
 #print("The lenght of services_list is:", len(services_list))
 #services_list.remove('ecr')
