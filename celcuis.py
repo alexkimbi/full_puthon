4fecf279fbe1647ba19de2deffec82545318b54b
Alex Kimbi <alexkimbi@hotmail.com>
2022-10-02 05:27:37 +0000

    added commends

--- celcuis.py
+++ celcuis.py
@@ -2,6 +2,6 @@
 
 #python implimentation goes here
 fahrenheit = float(input("What temp (in fahrenheit) would you like to convert to celsuis? "))
-celsuis = (fahrenheit - 32) * 5 / 9
+celsuis = (fahrenheit - 32) * 5 / 9 # formular to get the Celsuis
 
-print(fahrenheit, "F is",  round(celsuis, 2 ), "C")
+print(fahrenheit, "F is equal to ",  round(celsuis, 2 ), "C") # The round 2 makes it to round the anser to two decimal place
