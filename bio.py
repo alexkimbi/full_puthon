99120a14acdbf63ca076089847800ebff4313a9f
Alex Kimbi <alexkimbi@hotmail.com>
2022-10-02 03:23:33 +0000

    added 5th to 7th line

--- bio.py
+++ bio.py
@@ -1,4 +1,7 @@
 name = input("what is your name? ")
 color = input("What is your favorite color? ")
 age = int(input("how old are you? "))
-print(name "is " + str(age) + "and is a" + color "guy"and  + ".")
+#print(name "is " + str(age) + "and is a" + color "guy" and  + ".")
+print(name)
+print("is " + str(age) + " years old")
+print("and loves color "+ color + ".")
