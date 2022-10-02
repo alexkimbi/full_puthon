d14068563cd0caaa90aafb6fbbf37cf224dece35
alexkimbi <39074598+alexkimbi@users.noreply.github.com>
2022-10-02 00:28:20 -0400

    Update celcuis.py

--- celcuis.py
+++ celcuis.py
@@ -4,4 +4,4 @@
 fahrenheit = float(input("What temp (in fahrenheit) would you like to convert to celsuis? "))
 celsuis = (fahrenheit - 32) * 5 / 9
 
-print(fahrenheit, "F is",  round(celsuis, 2 ), "C")
+print(fahrenheit, "F is",  round(celsuis, 2 ), "C") # The round and 2 rounds the output to two decimal places
