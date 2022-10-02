29b1475a3b2a7c68b79411d9c0f10ee198b27fb5
Alex Kimbi <alexkimbi@hotmail.com>
2022-10-02 21:03:16 +0000

    Created Services file

---
+++ aws_services.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env pyhon3.7
+services_list = ['alex']
+services_list.append('ecr', 'ec2', 'lambda')
+print(services_list)
+#print("The lenght of services_list is:", len(services_list))
+#services_list.remove('ecr')
+
+###print("Deleting two specific services:")
+
