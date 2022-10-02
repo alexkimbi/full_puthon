e06f67afc988b226461727a8c1c8aa3b86dc55c8
Alex Kimbi <alexkimbi@hotmail.com>
2022-10-02 04:15:24 +0000

    added  float

--- celcuis.py
+++ celcuis.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3.7
 
 #python implimentation goes here
-fahrenheit = input("What temp (in fahrenheit) would you like to convert to celcuis? ")
+fahrenheit = float(input("What temp (in fahrenheit) would you like to convert to celcuis? "))
