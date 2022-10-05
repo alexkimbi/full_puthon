9041bd79d9565f0ead5e3882870b6805729bdf18
Alex Kimbi <alexkimbi@hotmail.com>
2022-10-05 06:45:36 +0000

    create using dictionary

---
+++ using_dictionary.py
@@ -0,0 +1,16 @@
+# 1) Set email vairiables to be empty dictionary 
+emails = {}
+
+assert emails == {}, f"Expected `emails` to be {{}} but got: {repr(emails)}
+
+# 2) Add users and emails 
+emails['ashley'] = 'ashley@example.com'
+emails['song'] = 'song@example.com'
+emails['Gilbert'] = 'gilbert@example.com'
+
+
+assert emails == {
+    "ashley": "ashley@example.com",
+    "song": "song@example.com",
+    "gilbert": "gilbert@example.com"
+}, f"Expected `emails` to be {{'ashley': 'ashley@example.com', 'song': 'song@example.com', 'gilbert': gilbert@example.com}}
