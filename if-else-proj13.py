31a3e9e500b2a997d5cb457a1dbe63cc4eb94f99
Alex Kimbi <alexkimbi@hotmail.com>
2022-10-07 05:02:12 +0000

    created project 13

---
+++ if-else-proj13.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3.7
+
+## Python implimentation goes here 
+
+number = int(input("What is your number:"))
+
+if number  >= 10:
+    print("You are right with the bigger number")
+elif number  <= 9:
+        print("That is not correct, the number is smaller")
+
+
+        
