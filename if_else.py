6214ce55ccac0de0ba589c5eea374e1bcd227188
Alex Kimbi <alexkimbi@hotmail.com>
2022-10-07 04:30:22 +0000

    creted if else file

---
+++ if_else.py
@@ -0,0 +1,15 @@
+first_name = input("What is your first name?:")
+last_name = input("What is your last name?:")
+if first_name !=  last_name:
+    print(f"Hello, {last_name} {first_name}!")
+else:
+    print(f"You can't have both, {last_name} and {first_name} thesame!")
+print("The length of first  name is:", len(first_name))
+print("The length of last   name is:", len(last_name))
+    
+
+
+        
+    
+        
+    
