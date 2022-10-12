3aeb9e913056e15b0136a9bddcc7c83442fcc49a
alexkimbi <alexkimbi@hotmail.com>
2022-10-12 00:58:52 -0400

    file to create S3 bucket using bot03

---
+++ boto3_1.py
@@ -0,0 +1,12 @@
+import boto3
+aws_resource=boto3.resource("s3")
+bucket=aws_resource.Bucket("boto3alexluit2")
+response = bucket.create(
+    ACL='private',
+    CreateBucketConfiguration={
+        'LocationConstraint': 'us-east-2'
+    },
+
+)
+print(response)
+ 
