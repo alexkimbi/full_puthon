7a10f16ed6ba3ce9510273d8b0795859668f9ee0
alexkimbi <alexkimbi@hotmail.com>
2022-10-13 23:58:50 -0400

    added

--- boto3_1.py
+++ boto3_1.py
@@ -1,6 +1,6 @@
 import boto3
 aws_resource=boto3.resource("s3")
-bucket=aws_resource.Bucket("boto3alexluit23")
+bucket=aws_resource.Bucket("boto3alexluit223")
 response = bucket.create(
     ACL='private',
     CreateBucketConfiguration={
