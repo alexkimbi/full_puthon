6cebc8621d67ae977f3e3b617b70990f4f2e3c4d
alexkimbi <alexkimbi@hotmail.com>
2022-10-17 21:55:03 -0400

    corrected the scripts

--- boto3_1.py
+++ boto3_1.py
@@ -10,5 +10,4 @@ response = bucket.create(
 )
 print(response)
 
-
  
--- search_S3_bucket.py
+++ search_S3_bucket.py
@@ -2,8 +2,6 @@
 import boto3
 s3 = boto3.client('s3')
 response = s3.list_buckets()
-buckets = response["buckets"]
-
-for bucket in resource.buckets.all():
-    print(bucket)
-
+print('Printing Existing buckets...:')
+for bucket in response['Buckets']:
+    print(f'  {bucket["Name"]}')
